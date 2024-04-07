28aa7e2b0e97872b03ee2bc881d152d196b6b815
mahoneybj <benmahoney10@gmail.com>
2024-04-08 09:42:27 +1200

    Led matrix working

---
+++ weather-fetch/symbols.h
@@ -0,0 +1,88 @@
+const uint32_t cloud[] = {
+	
+	0xf,
+	0x1082043,
+	0xfc000000,
+	66
+	
+};
+
+const uint32_t sun[] = {
+	
+	0x1f031,
+	0x82082083,
+	0x181f0000,
+	66
+	
+};
+
+const uint32_t heavyrain[] = {
+	
+	0xf010,
+	0x82043fc2,
+	0xa8550aa0,
+	66
+	
+};
+
+const uint32_t lightrain[] = {
+	0xf010,
+	0x82043fc1,
+	0x24248000,
+	66
+};
+
+const uint32_t suncloud[] = {
+	
+		0xf80f1cf2,
+		0x2e4187f0,
+		0x0,
+		66
+	
+};
+
+const uint32_t rainshower[] = {
+	
+	0xf80f1cf2,
+	0x2e4187f0,
+	0x2a0540a8,
+	66
+	
+};
+
+const uint32_t snow[] = {
+	
+		0xe011,
+		0x2083f82,
+		0xa80002a8,
+		66
+	
+};
+
+const uint32_t lightning[] = {
+	
+	0xf010820,
+	0x43fc1082,
+	0x10108084,
+	66
+	
+};
+
+const uint32_t wifi[] = {
+	
+		0x1f031844,
+		0x41b02080,
+		0xe0110040,
+		66
+	
+};
+
+const uint32_t nowifi[] = {
+	
+		0x20811,
+		0xa00400,
+		0xa0110208,
+		66
+	
+};
+
--- weather-fetch/weather-fetch.ino
+++ weather-fetch/weather-fetch.ino
@@ -6,17 +6,23 @@
 #include "ArduinoJson.h"
 #include "Arduino_LED_Matrix.h"
 #include <stdint.h>
-#include "symbols.h"  
+#include "symbols.h"
 
+ArduinoLEDMatrix matrix;
 LiquidCrystal_I2C lcd(0x27,  16, 2);
 char ssid[] = "Pig";           // Your WiFi network SSID
 char pass[] = "TommyInnit72";  // Your WiFi network password
+const int buttonPinH = 2; 
+const int buttonPinD = 3; 
+int buttonStateH = 0;
+int buttonStateD = 0;
 
 JsonArray dataHour;
 JsonArray dataDaily;
 
 void setup() {
   Serial.begin(9600);
+  matrix.begin();
   delay(5000);
 
   while (!Serial)
@@ -29,18 +35,26 @@ void setup() {
     delay(1000);
     Serial.println("Connecting...");
   }
-
+  
   Serial.println("Connected to WiFi");
+  matrix.loadFrame(wifi);
   lcd.init();
   lcd.backlight();
 
-  // Fetch JSON data
-  fetchHour();
-  fetchDaily();
+  currentWeather();
 }
 
 void loop() {
-  // Nothing to do here
+  buttonStateH = digitalRead(buttonPinH);
+  buttonStateD = digitalRead(buttonPinD);
+
+  if (buttonStateH == HIGH) {
+    fetchHour();
+  }
+  
+  if(buttonStateD == HIGH) {
+    fetchDaily();
+  }
 }
 
 void fetchHour() {
@@ -89,7 +103,10 @@ void fetchHour() {
       float temp = hour["temp"];
       // Extract time from datetime
       String time = hour["timestamp_local"].as<String>().substring(11);
+
+      int icon = hour["weather"]["code"];
       // Print or store the extracted data for each hour
+      Serial.print(icon);
       Serial.print("Time: ");
       Serial.println(time);
       Serial.print("Description: ");
@@ -97,32 +114,70 @@ void fetchHour() {
       Serial.print("Temperature: ");
       Serial.println(temp);
 
+      switch (icon) {
+        case 200 ... 299:
+          matrix.loadFrame(lightning);
+        break;
+        case 300 ... 399:
+          matrix.loadFrame(lightrain);
+        break;
+        case 500:
+          matrix.loadFrame(lightrain);
+        break;
+        case 501:
+          matrix.loadFrame(lightrain);
+        break;
+        case 502:
+          matrix.loadFrame(heavyrain);
+        break;
+        case 510 ... 523:
+          matrix.loadFrame(rainshower);
+        break;
+        case 600 ... 699:
+          matrix.loadFrame(snow);
+        break;
+        case 700 ... 799:
+          matrix.loadFrame(suncloud);
+        break;
+        case 800:
+          matrix.loadFrame(sun);
+        break;
+        case 801 ... 803:
+          matrix.loadFrame(suncloud);
+        break;
+        case 804:
+          matrix.loadFrame(cloud);
+        break;
+        default:
+    // statements
+        break;
+      }
       lcd.setCursor(0,0);
       lcd.print("Time: " + time);
       lcd.setCursor(0,1);
       lcd.print("Temp: " + String(temp) + "c");
       delay(2000);
       lcd.clear();
-
-
-
     }
 
     // Close connection
     client.stop();
   } else {
     Serial.println("Connection failed");
+    matrix.loadFrame(nowifi);
+
     Serial.println(client.getWriteError());
   }
+  currentWeather();
 }
 
 void fetchDaily() {
-  WiFiSSLClient client; // Change to WiFiClientSecure for HTTPS
+  WiFiSSLClient client; 
 
   const char server[] = "api.weatherbit.io";
   const char* resource = "/v2.0/forecast/daily?lat=-45.8741&lon=170.5036&key=dd2f30b33a644bac846c95daa1975889&days=5";
 
-  if (client.connect(server, 443)) {  // Use port 443 for HTTPS
+  if (client.connect(server, 443)) {  
     client.print("GET ");
     client.print(resource);
     client.println(" HTTP/1.1");
@@ -161,7 +216,7 @@ void fetchDaily() {
       const char* description = day["weather"]["description"];
       float maxTemp = day["max_temp"];
       float minTemp = day["min_temp"];
-
+      int icon = day["weather"]["code"];
       // Extract time from datetime
       String date = day["datetime"].as<String>();
       // Print or store the extracted data for each hour
@@ -171,6 +226,47 @@ void fetchDaily() {
       Serial.println(description);
       Serial.print("Max temp: ");
       Serial.println(maxTemp);
+
+      switch (icon) {
+        case 200 ... 299:
+          matrix.loadFrame(lightning);
+        break;
+        case 300 ... 399:
+          matrix.loadFrame(lightrain);
+        break;
+        case 500:
+          matrix.loadFrame(lightrain);
+        break;
+        case 501:
+          matrix.loadFrame(lightrain);
+        break;
+        case 502:
+          matrix.loadFrame(heavyrain);
+        break;
+        case 510 ... 523:
+          matrix.loadFrame(rainshower);
+        break;
+        case 600 ... 699:
+          matrix.loadFrame(snow);
+        break;
+        case 700 ... 799:
+          matrix.loadFrame(suncloud);
+        break;
+        case 800:
+          matrix.loadFrame(sun);
+        break;
+        case 801 ... 803:
+          matrix.loadFrame(suncloud);
+        break;
+        case 804:
+          matrix.loadFrame(cloud);
+        break;
+        default:
+    // statements
+        break;
+      }
+
+
 
       lcd.setCursor(0,0);
       lcd.print(date);
@@ -184,6 +280,118 @@ void fetchDaily() {
     client.stop();
   } else {
     Serial.println("Connection failed");
+    matrix.loadFrame(nowifi);
+    Serial.println(client.getWriteError());
+  }
+  currentWeather();
+}
+  void currentWeather(){
+WiFiSSLClient client; 
+
+  const char server[] = "api.weatherbit.io";
+  const char* resource = "/v2.0/forecast/hourly?lat=-45.8741&lon=170.5036&key=dd2f30b33a644bac846c95daa1975889&hours=6";
+
+  if (client.connect(server, 443)) {  // Use port 443 for HTTPS
+    client.print("GET ");
+    client.print(resource);
+    client.println(" HTTP/1.1");
+    client.println("Host: api.weatherbit.io");
+    client.println("Connection: close");
+    client.println();  // End of HTTP headers
+
+    // Read response
+    bool headerPassed = false;
+    String jsonResponse = "";
+    while (client.connected()) {
+      String line = client.readStringUntil('\n');
+      // Check if we reached the end of the headers
+      if (line == "\r") {
+        headerPassed = true;
+      }
+      // If headers are passed, concatenate JSON data
+      if (headerPassed && line != "\r") {
+        jsonResponse += line;
+      }
+    }
+
+    // Parse JSON response
+    DynamicJsonDocument doc(1024); // Adjust the size as per your JSON response size
+    DeserializationError error = deserializeJson(doc, jsonResponse);
+    if (error) {
+      Serial.print("deserializeJson() failed: ");
+      Serial.println(error.c_str());
+      return;
+    }
+
+    // Extract data from JSON response
+    dataHour = doc["data"];
+    for (int i = 0; i < 1; i++) {
+      JsonObject hour = dataHour[i];
+      const char* description = hour["weather"]["description"];
+      float temp = hour["temp"];
+      // Extract time from datetime
+      String time = hour["timestamp_local"].as<String>().substring(11);
+
+      int icon = hour["weather"]["code"];
+      // Print or store the extracted data for each hour
+      Serial.print(icon);
+      Serial.print("Time: ");
+      Serial.println(time);
+      Serial.print("Description: ");
+      Serial.println(description);
+      Serial.print("Temperature: ");
+      Serial.println(temp);
+
+      switch (icon) {
+        case 200 ... 299:
+          matrix.loadFrame(lightning);
+        break;
+        case 300 ... 399:
+          matrix.loadFrame(lightrain);
+        break;
+        case 500:
+          matrix.loadFrame(lightrain);
+        break;
+        case 501:
+          matrix.loadFrame(lightrain);
+        break;
+        case 502:
+          matrix.loadFrame(heavyrain);
+        break;
+        case 510 ... 523:
+          matrix.loadFrame(rainshower);
+        break;
+        case 600 ... 699:
+          matrix.loadFrame(snow);
+        break;
+        case 700 ... 799:
+          matrix.loadFrame(suncloud);
+        break;
+        case 800:
+          matrix.loadFrame(sun);
+        break;
+        case 801 ... 803:
+          matrix.loadFrame(suncloud);
+        break;
+        case 804:
+          matrix.loadFrame(cloud);
+        break;
+        default:
+    // statements
+        break;
+      }
+      lcd.setCursor(0,0);
+      lcd.print("Time: " + time);
+      lcd.setCursor(0,1);
+      lcd.print("Temp: " + String(temp) + "c");
+      delay(2000);
+    }
+
+    // Close connection
+    client.stop();
+  } else {
+    Serial.println("Connection failed");
+    matrix.loadFrame(nowifi);
     Serial.println(client.getWriteError());
   }
 }
